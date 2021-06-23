96fc3bb9e4b0792c81e15b0680e0e85b3374eab0
Massafelli <lmassafelli@yahoo.com.br>
2021-06-23 13:06:46 -0300

    Fix commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const convert = require("./lib/convert");
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/cotacao', (req, res) => {
+    const { cotation, amount } = req.query;
+    if (cotation && amount) {
+
+        const convertion = convert.convert(cotation, amount)
+        res.render("cotacao", {
+            err: false,
+            convertion: convert.toMoney(convertion),
+            cotation: convert.toMoney(cotation),
+            amount: convert.toMoney(amount)
+        });
+    } else {
+        res.render('cotacao', {
+            err: 'Invalid values'
+        })
+    }
+})
+
+app.listen(3000, (err) => {
+    if (err) {
+
+        console.log('Não foi possivel iniciar');
+    } else {
+        console.log('Server running');
+    }
+})
